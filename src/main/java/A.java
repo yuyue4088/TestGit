750d7bffe65153c46927a17758fe4b9c18b09a24
yuyue <3607499799@qq.com>
2023-02-09 19:48:44 +0800

    first commit 3

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,6 +2,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello3");
 
     }
 }
