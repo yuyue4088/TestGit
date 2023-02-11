5fdcc13a756605d2091379b3638b60c1d320af17
yuyue <3607499799@qq.com>
2023-02-11 17:30:59 +0800

    hot-fix 2

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -7,6 +7,7 @@ public class A {
         System.out.println("hello4");
         System.out.println("hello5");
         System.out.println("hotfix---hello5");
+        System.out.println("hotfix---hello6");
 
 
     }
